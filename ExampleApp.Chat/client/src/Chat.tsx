9a6dd6065831614060685880696c67b87d870f4e
Alex Uldahl <alex@uldahl.dk>
2026-01-24 22:36:15 +0100

    Update Chat.tsx

--- ExampleApp.Chat/client/src/Chat.tsx
+++ ExampleApp.Chat/client/src/Chat.tsx
@@ -4,30 +4,27 @@ import {ChatClient} from "./generated-ts-client.ts";
 
 const BASE_URL = 'http://localhost:5000';
 
-let es = new EventSource(BASE_URL)
 const chatClient = new ChatClient(BASE_URL)
 export default function Chat() {
 
     const [ messages, setMessages] = useState<any[]>([])
 
     useEffect(() => {
+        const channels = ['rooms/123/messages', 'rooms/123/typing'];
+        const es = new EventSource(`${BASE_URL}?channels=${encodeURIComponent(channels.join(','))}`);
 
-        const channels = ['room:123:messages', 'room:123:typing'];
-         es = new EventSource(`${BASE_URL}?channels=${encodeURIComponent(channels.join(','))}`);
-        if(es && es.readyState == es.OPEN && chatClient) {
-            es.addEventListener('room:123:messages', (dto) => {
-                setMessages((prev) => [...prev, dto])
-            })
+        es.addEventListener('rooms/123/messages', (event) => {
+            setMessages((prev) => [...prev, JSON.parse(event.data)]);
+        });
 
-        }
-
-    }, [es]);
+        return () => es.close();
+    }, []);
 
     return <>
 
 
         <button onClick={() => {
-            chatClient.sendMessage("room:123", {
+            chatClient.sendMessage("123", {
                 author: "bob",
                 content: "hi"
             }).then(r => {
