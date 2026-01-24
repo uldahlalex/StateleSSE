ef6cdebb440a897c8656103687951589b2fda95d
Alex Uldahl <alex@uldahl.dk>
2026-01-24 21:57:49 +0100

    Update Chat.tsx

--- ExampleApp.Chat/client/src/Chat.tsx
+++ ExampleApp.Chat/client/src/Chat.tsx
@@ -3,13 +3,15 @@ import {ChatClient} from "./generated-ts-client.ts";
 
 const BASE_URL = 'http://localhost:5000';
 
-const es = new EventSource(BASE_URL)
+let es = new EventSource(BASE_URL)
 const chatClient = new ChatClient(BASE_URL)
 export default function Chat() {
 
     const [ messages, setMessages] = useState<any[]>([])
 
     useEffect(() => {
+        const channels = ['room:123:messages', 'room:123:typing'];
+         es = new EventSource(`${BASE_URL}?channels=${encodeURIComponent(channels.join(','))}`);
         if(es && es.readyState == es.OPEN && chatClient) {
             es.addEventListener('room:123:messages', (dto) => {
                 setMessages((prev) => [...prev, dto])
@@ -17,10 +19,21 @@ export default function Chat() {
 
         }
 
-    }, []);
+    }, [es]);
 
     return <>
 
+
+        <button onClick={() => {
+            chatClient.sendMessage("room:123", {
+                author: "bob",
+                content: "hi"
+            }).then(r => {
+
+            })
+        }}>
+            add
+        </button>
         {
             JSON.stringify(messages)
         }
